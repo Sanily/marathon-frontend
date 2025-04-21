f8993f169db5d694ad3c5b0aaa3972e6ade2cdc2
Sanily <1197356696@qq.com>
2025-04-21 10:27:31 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    // 端口配置
+    port: 8080,
+  }
+})
